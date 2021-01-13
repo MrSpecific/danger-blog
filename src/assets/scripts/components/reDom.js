d259fb3837e52385430939bd513c9f612dabbeeb
Will Christenson <will@wlcr.io>
2021-01-13 14:40:57 -0800

    Add Re:DOM

---
+++ src/assets/scripts/components/reDom.js
@@ -0,0 +1,18 @@
+import { el, mount } from "redom";
+
+const init = () => {
+  const reDomContainer = document.querySelector('#redom-container');
+
+  if (!reDomContainer) return;
+
+  const hello = el("h2", "Hello world from Re:DOM!");
+  mount(reDomContainer, hello);
+}
+
+const reDom = {
+  el,
+  mount,
+  init: init(),
+};
+
+export default reDom;
--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,13 +1,15 @@
 import { navigator } from "@hotwired/turbo";
 // import Turbo from "@hotwired/turbo";
 import collectJSON from './utilities/collectJSON';
+import reDom from './components/reDom';
 
 const build = collectJSON('#build-json');
 
+// Prevent Turbolinks in dev mode
 if (build.env === "development") {
   document.documentElement.addEventListener('turbo:click', (event) => {
     event.preventDefault();
   });
 }
 // Turbo.visit();
-console.log('Hello world', build.env, navigator);
+console.log('Hello world', build.env, navigator, reDom);
